d1f93965cdbb827f11945a9152134dc4b05834ec
Siddhant Chandiwal <chandiwal.s@husky.neu.edu>
2016-09-13 23:56:47 -0400

    Adding Homework_1_First_Commit

---
+++ Homework_1/src/Business/Resume.java
@@ -0,0 +1,160 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business;
+
+/**
+ *
+ * @author siddhant
+ */
+public class Resume {
+    
+    private String firstName;
+    private String lastName;
+    private String streetln1;
+    private String streetln2;
+    private String city;
+    private String zipCode;
+    private String country;
+    private String affiliation;
+    private String careerObjectiveStatement;
+    private String degree1Name;
+    private String degree1Year;
+    private String degree2Name;
+    private String degree2Year;
+   
+    private String totalWorkExperienceInYears;
+     private String contactNo;
+    private String emailId;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getStreetln1() {
+        return streetln1;
+    }
+
+    public void setStreetln1(String streetln1) {
+        this.streetln1 = streetln1;
+    }
+
+    public String getStreetln2() {
+        return streetln2;
+    }
+
+    public void setStreetln2(String streetln2) {
+        this.streetln2 = streetln2;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+    
+     public String getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getAffiliation() {
+        return affiliation;
+    }
+
+    public void setAffiliation(String affiliation) {
+        this.affiliation = affiliation;
+    }
+
+    public String getCareerObjectiveStatement() {
+        return careerObjectiveStatement;
+    }
+
+    public void setCareerObjectiveStatement(String careerObjectiveStatement) {
+        this.careerObjectiveStatement = careerObjectiveStatement;
+    }
+
+    public String getDegree1Name() {
+        return degree1Name;
+    }
+
+    public void setDegree1Name(String degree1Name) {
+        this.degree1Name = degree1Name;
+    }
+
+    public String getDegree1Year() {
+        return degree1Year;
+    }
+
+    public void setDegree1Year(String degree1Year) {
+        this.degree1Year = degree1Year;
+    }
+
+    public String getDegree2Name() {
+        return degree2Name;
+    }
+
+    public void setDegree2Name(String degree2Name) {
+        this.degree2Name = degree2Name;
+    }
+
+    public String getDegree2Year() {
+        return degree2Year;
+    }
+
+    public void setDegree2Year(String degree2Year) {
+        this.degree2Year = degree2Year;
+    }
+
+    public String getContactNo() {
+        return contactNo;
+    }
+
+    public void setContactNo(String contactNo) {
+        this.contactNo = contactNo;
+    }
+
+    public String getTotalWorkExperienceInYears() {
+        return totalWorkExperienceInYears;
+    }
+
+    public void setTotalWorkExperienceInYears(String totalWorkExperienceInYears) {
+        this.totalWorkExperienceInYears = totalWorkExperienceInYears;
+    }
+
+    public String getEmailId() {
+        return emailId;
+    }
+
+    public void setEmailId(String emailId) {
+        this.emailId = emailId;
+    }
+    
+}
