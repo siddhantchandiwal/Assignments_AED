80345c7e7618d64134e5e4fa7984ac884c13aba7
Siddhant Chandiwal <chandiwal.s@husky.neu.edu>
2016-09-17 11:52:51 -0400

    Adding Lab2 Vitals Project

---
+++ Lab2/src/business/VitalSignHistory.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package business;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Siddhant
+ */
+public class VitalSignHistory {
+    private ArrayList<VitalSigns> vitalSignHistory;
+    
+    public VitalSignHistory(){
+        vitalSignHistory=new ArrayList<VitalSigns>();
+    }
+
+    public ArrayList<VitalSigns> getVitalSignHistory() {
+        return vitalSignHistory;
+    }
+
+    public void setVitalSignHistory(ArrayList<VitalSigns> vitalSignHistory) {
+        this.vitalSignHistory = vitalSignHistory;
+    }
+    
+    public VitalSigns addVitals(){
+        VitalSigns vs=new VitalSigns();
+        vitalSignHistory.add(vs);
+        return vs;
+    }
+    
+    public void deleteVitals(VitalSigns v){
+         vitalSignHistory.remove(v);
+    } 
+}
